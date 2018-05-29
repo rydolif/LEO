1bfb5551848ec1c3b864f2e9a133bebae129b6e9
rydolif <rudolifrudolif@gmail.com>
2018-05-29 13:07:07 +0300

    first commit

---
+++ app/js/common.js
@@ -0,0 +1,176 @@
+$(function() {
+
+$(".preloader").delay(1000).fadeOut();
+
+$(".nav").on("click","a", function (event) {
+  event.preventDefault();
+    var id  = $(this).attr('href'),
+    top = $(id).offset().top;
+  $('body,html').animate({scrollTop: top - 90}, 'slow', 'swing');
+});
+
+//------------------------------гамбургер-----------------------------
+$('.hamburger--3dx').click(function() {
+  $(this).toggleClass('is-active');
+  $('nav').toggleClass('nav-active');
+  $('header').toggleClass('header-menu');
+});
+
+//-------------------------------попандер---------------------------------------
+  $('.modal').popup({transition: 'all 0.3s'});
+
+//------------------------------------form-------------------------------------------
+	$('input[type="tel"]').mask('+0 (000) 000-00-00');
+
+	jQuery.validator.addMethod("phoneno", function(phone_number, element) {
+	   return this.optional(element) || phone_number.match(/\+[0-9]{1}\s\([0-9]{3}\)\s[0-9]{3}-[0-9]{2}-[0-9]{2}/);
+	}, "Введите Ваш телефон");
+
+  $(".order-form").validate({
+    messages: {
+      name: "Введите ваше Имя",
+      phone: "Введите ваш телефон",
+    },
+    rules: {
+      "phone": {
+        required: true,
+        phoneno: true
+      }
+    },
+    submitHandler: function(form) {
+      var t = {
+        name: jQuery(".order-form").find("input[name=name]").val(),
+        phone: jQuery(".order-form").find("input[name=phone]").val(),
+        subject: jQuery(".order-form").find("input[name=subject]").val()
+      };
+      ajaxSend('.order-form', t);
+    }
+  });
+
+  $(".presentation-form").validate({
+    messages: {
+      name: "Введите ваше Имя",
+      phone: "Введите ваш телефон",
+    },
+    rules: {
+      "phone": {
+        required: true,
+        phoneno: true
+      }
+    },
+    submitHandler: function(form) {
+      var t = {
+        name: jQuery(".presentation-form").find("input[name=name]").val(),
+        phone: jQuery(".presentation-form").find("input[name=phone]").val(),
+        subject: jQuery(".presentation-form").find("input[name=subject]").val()
+      };
+      ajaxSend('.presentation-form', t);
+    }
+  });
+
+  $(".consultation-form").validate({
+    messages: {
+      name: "Введите ваше Имя",
+      phone: "Введите ваш телефон",
+    },
+    rules: {
+      "phone": {
+        required: true,
+        phoneno: true
+      }
+    },
+    submitHandler: function(form) {
+      var t = {
+        name: jQuery(".consultation-form").find("input[name=name]").val(),
+        phone: jQuery(".consultation-form").find("input[name=phone]").val(),
+        subject: jQuery(".consultation-form").find("input[name=subject]").val()
+      };
+      ajaxSend('.consultation-form', t);
+    }
+  });
+
+  function ajaxSend(formName, data) {
+    jQuery.ajax({
+      type: "POST",
+      url: "sendmail.php",
+      data: data,
+      success: function() {
+        $(".modal").popup("hide");
+        $("#thanks").popup("show");
+        setTimeout(function() {
+          $(formName).trigger('reset');
+        }, 2000);
+      }
+    });
+  }
+
+//----------------------------------------fixed----------------------------------
+  $(window).scroll(function(){
+      if($(this).scrollTop()>50){
+          $('.header').addClass('header-active');
+      }
+      else if ($(this).scrollTop()<50){
+          $('.header').removeClass('header-active');
+      }
+  });
+
+
+( function( window, document )
+{
+  'use strict';
+
+  var file     = 'img/symbols.html',
+      revision = 1.2;
+
+  if( !document.createElementNS || !document.createElementNS( 'http://www.w3.org/2000/svg', 'svg' ).createSVGRect )
+      return true;
+
+  var isLocalStorage = 'localStorage' in window && window[ 'localStorage' ] !== null,
+      request,
+      data,
+      insertIT = function()
+      {
+          document.body.insertAdjacentHTML( 'afterbegin', data );
+      },
+      insert = function()
+      {
+          if( document.body ) insertIT();
+          else document.addEventListener( 'DOMContentLoaded', insertIT );
+      };
+
+  if( isLocalStorage && localStorage.getItem( 'inlineSVGrev' ) == revision )
+  {
+    data = localStorage.getItem( 'inlineSVGdata' );
+    if( data )
+    {
+        insert();
+        return true;
+    }
+  }
+
+  try
+  {
+    request = new XMLHttpRequest();
+    request.open( 'GET', file, true );
+    request.onload = function()
+      {
+        if( request.status >= 200 && request.status < 400 )
+          {
+            data = request.responseText;
+            insert();
+            if( isLocalStorage )
+            {
+              localStorage.setItem( 'inlineSVGdata',  data );
+              localStorage.setItem( 'inlineSVGrev',   revision );
+            }
+        }
+    }
+    request.send();
+  }
+  catch( e ){}
+
+}( window, document ) );
+
+
+});
+
